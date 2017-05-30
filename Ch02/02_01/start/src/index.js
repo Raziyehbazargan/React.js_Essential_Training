cf3637a01031aa1c893a1315cf693819ba225f6b
Raziyeh Bazargan <raziyeh.bazargan@eventcore.com>
2017-05-30 15:43:36 -0700

    Loading with Webpack

--- Ch02/02_01/start/src/index.js
+++ Ch02/02_01/start/src/index.js
@@ -17,7 +17,7 @@ render(
   <h1
     id='title'
     className='header'
-    style={{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}}>Hello World!
+    style={{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}}>Hello World!!
   </h1>,
   document.getElementById('react-container')
 )
