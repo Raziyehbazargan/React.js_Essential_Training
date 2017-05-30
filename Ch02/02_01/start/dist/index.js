b05ea75f67a0ff15419eb6817f60a792a97d9af3
Raziyeh Bazargan <raziyeh.bazargan@eventcore.com>
2017-05-30 13:10:39 -0700

    used babel inline broswer transpiler

--- Ch02/02_01/start/dist/index.js
+++ Ch02/02_01/start/dist/index.js
@@ -1,19 +1,23 @@
 const { createElement } = React
 const { render } = ReactDOM
 
-const style = {
-  backgroundColor: 'orange',
-  color: 'white',
-  fontFamily: 'verdana'
-
-}
-const title = createElement(
-  'h1',
-  {id: 'title', className: 'header', style: style},
-  'Hello World'
-)
+// const style = {
+//   backgroundColor: 'orange',
+//   color: 'white',
+//   fontFamily: 'verdana'
+//
+// }
+// const title = createElement(
+//   'h1',
+//   {id: 'title', className: 'header', style: style},
+//   'Hello World'
+// )
 
 render(
-  title,
+  <h1
+    id='title'
+    className='header'
+    style={{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}}>Hello World!
+  </h1>,
   document.getElementById('react-container')
 )
