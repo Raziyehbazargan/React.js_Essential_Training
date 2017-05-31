a319451606aa80befcf0de602adce30d09345c74
Raziyeh Bazargan <raziyeh.bazargan@eventcore.com>
2017-05-31 11:59:15 -0700

    removed additional folders

--- Ch03/03_02/start/src/index.js
+++ Ch03/03_02/start/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { SkiDayCount } from './components/ski-day-count'
+window.React = React
 
 render(
-	<div>
-	</div>,
+	<SkiDayCount />,
 	document.getElementById('react-container')
 )
