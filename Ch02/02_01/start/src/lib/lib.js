f6a89f06481524e1510d0082f41ee8ae170b94a8
Raziyeh Bazargan <raziyeh.bazargan@eventcore.com>
2017-05-31 10:17:48 -0700

    fixed error for path

--- Ch02/02_01/start/src/lib/lib.js
+++ Ch02/02_01/start/src/lib/lib.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import text from './titles.json';
-
-import './stylesheets/goodbye.scss';
-import './stylesheets/hello.css';
+import React from 'react'
+import text from './titles.json'
+import '../stylesheets/goodbye.scss'
+import '../stylesheets/hello.css'
 
 export const hello = (
-  <h1
-    id='title'
-    className='hello'>{text.hello}
-  </h1>
+	<h1 id='title'
+		className='hello'>
+		{text.hello}
+	</h1>
 )
 
-
 export const goodbye = (
-  <h1
-    id='title'
-    className='goodbye'>{text.goodbye}
-  </h1>
+	<h1 id='title'
+		className='goodbye'>
+		{text.goodbye}
+	</h1>
 )
--- Ch02/02_01/start/webpack.config.js
+++ Ch02/02_01/start/webpack.config.js
@@ -1,40 +1,40 @@
-var Webpack = require("webpack");
+var webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: "dist/assets",
-    filename: "bundle.js",
-    publicPath: "assets"
-  },
-  devServer: {
-    inline: true,
-    contentBase: './dist',
-    port: 4000
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        loader: ["babel-loader"],
-        query: {
-          presets: ["latest","stage-0","react"]
-        }
-      },
-      {
-        test: /\.json$/,
-        exclude: /(node_modules)/,
-        loader: "json-loader"
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader!autoprefixer-loader'
-      },
-      {
-        test: /\.scss$/,
-        loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
-      }
-    ]
-  }
+	entry: "./src/index.js",
+	output: {
+		path: "dist/assets",
+		filename: "bundle.js",
+		publicPath: "assets"
+	},
+	devServer: {
+		inline: true,
+		contentBase: './dist',
+		port: 4000
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: ["babel-loader"],
+				query: {
+					presets: ["latest", "stage-0", "react"]
+				}
+			},
+			{
+				test: /\.json$/,
+				exclude: /(node_modules)/,
+				loader: "json-loader"
+			},
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader!autoprefixer-loader'
+			},
+			{
+				test: /\.scss$/,
+				loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
+			}
+		]
+	}
 }
