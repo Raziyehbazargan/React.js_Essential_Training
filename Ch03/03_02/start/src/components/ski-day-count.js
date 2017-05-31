cb0506c0c45d465557d0b4c80df017d25116bdaa
Raziyeh Bazargan <raziyeh.bazargan@eventcore.com>
2017-05-31 13:13:33 -0700

    add react icons

--- Ch03/03_02/start/src/components/ski-day-count.js
+++ Ch03/03_02/start/src/components/ski-day-count.js
@@ -1,4 +1,7 @@
 import '../stylesheets/ui.scss'
+import Terrain from 'react-icons/lib/md/terrain'
+import SnowFlake from 'react-icons/lib/ti/weather-snow'
+import Calendar from 'react-icons/lib/fa/calendar'
 
 //Component Methods
 const percentToDecimal = (decimal) => {
@@ -13,14 +16,17 @@ export const SkiDayCount = (props) => (
 	<div className="ski-day-count">
 		<div className="total-days">
       <span>{props.total}</span>
+      <Calendar />
 			<span>days</span>
 		</div>
 		<div className="powder-days">
       <span>{props.powder}</span>
+      <SnowFlake />
 			<span>days</span>
 		</div>
 		<div className="backcountry-days">
       <span>{props.backcountry}</span>
+      <Terrain />
 			<span>days</span>
 		</div>
     <div>
