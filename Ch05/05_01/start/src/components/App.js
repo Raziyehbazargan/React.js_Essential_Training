3126ca16dd7a247eef65d7623dd4e194c493df13
Raziyeh Bazargan <raziyeh.bazargan@eventcore.com>
2017-06-01 10:54:10 -0700

    convert to ES6

--- Ch05/05_01/start/src/components/App.js
+++ Ch05/05_01/start/src/components/App.js
@@ -1,53 +1,45 @@
-import { createClass } from 'react'
-import { SkiDayList } from './SkiDayList'
-import { SkiDayCount } from './SkiDayCount'
+import { Component } from 'react';
+import { SkiDayList } from '../components/ski-day-list'
+import { SkiDayCount } from '../components/ski-day-count'
 
-export const App = createClass({
-	getInitialState() {
-		return {
-			allSkiDays: [
-			{
-				resort: "Squaw Valley",
-				date: new Date("1/2/2016"),
-				powder: true,
-				backcountry: false
-			},
-			{
-				resort: "Kirkwood",
-				date: new Date("3/28/2016"),
-				powder: false,
-				backcountry: false
-			},
-			{
-				resort: "Mt. Tallac",
-				date: new Date("4/2/2016"),
-				powder: false,
-				backcountry: true
-			}
-		]
-		}
-	},
-	countDays(filter) {
-		const { allSkiDays } = this.state
-		return allSkiDays.filter(
-			(day) => (filter) ? day[filter] : day).length
-	},
-	render() {
-		return (
-			<div className="app">
-				<SkiDayList days={this.state.allSkiDays}/>
-				<SkiDayCount total={this.countDays()}
-							 powder={this.countDays(
-							 		"powder"
-							 	)}
-							 backcountry={this.countDays(
-							 		"backcountry"
-							 	)}/>
-			</div>
-		)
-	}
-})
+export class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      allSkiDays: [
+       {
+         resort: "Squaw Valley",
+         date: new Date("1/2/2016"),
+         powder: true,
+         backcountry: false
+       },
+       {
+         resort: "Kirkwood",
+         date: new Date("3/2/2016"),
+         powder: false,
+         backcountry: false
+       },
+       {
+         resort: "Mt. Tallac",
+         date: new Date("4/2/2016"),
+         powder: false,
+         backcountry: true
+       }
+     ]
+    }
+  }
 
-
-
-
+  countDays(filter) {
+    //ES6
+    //const { allSkiDays } = this.state; then no need this.state.allSkiDays: allSkiDays
+    return this.state.allSkiDays.filter(day => (filter) ? day[filter] : day).length
+  }
+  render() {
+    return (
+      <div className="app">
+        <SkiDayList days={this.state.allSkiDays} />
+        <SkiDayCount  total={this.countDays()} powder={this.countDays("powder")} backcountry={this.countDays("backcountry")}/>
+      </div>
+    )
+  }
+}
