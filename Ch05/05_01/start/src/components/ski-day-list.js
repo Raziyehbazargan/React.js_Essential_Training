cd0bab3fce7d4914b8d38faa544b4bd2b2ab3af3
Raziyeh Bazargan <raziyeh.bazargan@eventcore.com>
2017-06-01 11:22:16 -0700

    adding router

--- Ch05/05_01/start/src/components/ski-day-list.js
+++ Ch05/05_01/start/src/components/ski-day-list.js
@@ -1,8 +1,9 @@
 import Terrain from 'react-icons/lib/md/terrain'
 import SnowFlake from 'react-icons/lib/ti/weather-snow'
 import Calendar from 'react-icons/lib/fa/calendar'
-import { SkiDayRow } from './SkiDayRow'
-import { PropTypes } from 'react'
+import { SkiDayRow } from './ski-day-row'
+import PropTypes from 'prop-types';
+
 
 export const SkiDayList = ({days}) => (
 	<table>
@@ -17,38 +18,23 @@ export const SkiDayList = ({days}) => (
 		<tbody>
 			{days.map((day, i) =>
 				<SkiDayRow key={i}
-						   {...day}/>	
+						   {...day}/>
 				)}
 		</tbody>
 
 	</table>
 )
 
+//resort={day.resort} date={day.date} powder={day.powder} backcountry={day.backcountry} it's eaul to {...days}
+
 SkiDayList.propTypes = {
 	days: function(props) {
-		if(!Array.isArray(props.days)) {
-			return new Error(
-				"SkiDayList should be an array"	
-				)
-		} else if(!props.days.length) {
-			return new Error(
-				"SkiDayList must have at least one record"
-				)
+		if (!Array.isArray(props.days)) {
+			return new Error ("Should be an array")
+		} else if (!props.days.length) {
+			return new Error("must have at least one record")
 		} else {
 			return null
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ Ch05/05_01/start/src/components/whoops-404.js
@@ -0,0 +1,7 @@
+// a stateless component
+
+export const Whoops404 = () => {
+  <div>
+    <h1>Whoops, route not found</h1>
+  </div>
+}
