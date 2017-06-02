c942600bf56d849f8c22062ac621e8f391e146b4
Raziyeh Bazargan <raziyeh.bazargan@eventcore.com>
2017-06-02 13:09:19 -0700

    add form component - default values and propTypes

--- Ch06/06_01/start/src/components/AddDayForm.js
+++ Ch06/06_01/start/src/components/AddDayForm.js
@@ -1,8 +1,35 @@
-import { PropTypes } from 'react'
+import { Component } from 'react'
+import { PropTypes } from 'prop-types'
 
-export const AddDayForm = () => (
-	<h1>Add A Day</h1>
-)
+export class AddDayForm extends Component {
+	render() {
+
+		const { resort, date, powder, backcountry } = this.props
+
+		return (
+			<form className="add-day-form">
+			  <label htmlFor="resort">Report Name</label>
+				<input id="resort" type="text" defaultValue={resort} required />
+
+				<label htmlFor="date">Date</label>
+				<input id="date" type="date" defaultValue={date} required />
+
+				<label htmlFor="powder">Powder Day</label>
+				<input id="powder" defaultChecked={powder} type="checkbox" />
+
+				<label htmlFor="backcountry">Backcountry Name</label>
+				<input id="backcountry" type="checkbox" defaultChecked={backcountry} required />
+			</form>
+		)
+	}
+}
+
+AddDayForm.defaultProps = {
+	resort: "Kirkwood",
+	date: "2017-02-01",
+	powder: true,
+	backcountry: false
+}
 
 AddDayForm.propTypes = {
 	resort: PropTypes.string.isRequired,
