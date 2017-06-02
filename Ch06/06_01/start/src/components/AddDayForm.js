6fbc54e17888b499ca6b96dcbd6cd13b147505ea
Raziyeh Bazargan <raziyeh.bazargan@eventcore.com>
2017-06-02 13:20:21 -0700

    working with refs in form component

--- Ch06/06_01/start/src/components/AddDayForm.js
+++ Ch06/06_01/start/src/components/AddDayForm.js
@@ -2,23 +2,58 @@ import { Component } from 'react'
 import { PropTypes } from 'prop-types'
 
 export class AddDayForm extends Component {
-	render() {
 
+	constructor(props) {
+		super(props)
+		this.submit = this.submit.bind(this)
+	}
+
+	submit(e) {
+		e.preventDefault()
+		console.log('resort', this.refs.resort.value)
+		console.log('resort', this.refs.date.value)
+		console.log('resort', this.refs.powder.checked)
+		console.log('resort', this.refs.backcountry.checked)
+	}
+	render() {
 		const { resort, date, powder, backcountry } = this.props
 
 		return (
-			<form className="add-day-form">
+			<form onSubmit={this.submit} className="add-day-form">
 			  <label htmlFor="resort">Report Name</label>
-				<input id="resort" type="text" defaultValue={resort} required />
+				<input id="resort"
+					type="text"
+					defaultValue={resort}
+					ref="resort"
+					required />
 
 				<label htmlFor="date">Date</label>
-				<input id="date" type="date" defaultValue={date} required />
+				<input id="date"
+					type="date"
+					defaultValue={date}
+					ref="date"
+					required />
 
-				<label htmlFor="powder">Powder Day</label>
-				<input id="powder" defaultChecked={powder} type="checkbox" />
+				<div>
+					<input
+						id="powder"
+						defaultChecked={powder}
+						ref="powder"
+						type="checkbox" />
+						<label htmlFor="powder">Powder Day</label>
+				</div>
 
-				<label htmlFor="backcountry">Backcountry Name</label>
-				<input id="backcountry" type="checkbox" defaultChecked={backcountry} required />
+				<div>
+					<input
+						id="backcountry"
+						type="checkbox"
+						defaultChecked={backcountry}
+						ref="backcountry"
+						required />
+						<label htmlFor="backcountry">Backcountry Name</label>
+
+				</div>
+				<button>Add day</button>
 			</form>
 		)
 	}
