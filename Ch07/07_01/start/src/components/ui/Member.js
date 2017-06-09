5bbc13562661d3d2ab2cec972131da4e50c18e7e
Raziyeh Bazargan <raziyeh.bazargan@eventcore.com>
2017-06-09 12:01:09 -0700

    member and memberlist component

--- Ch07/07_01/start/src/components/ui/Member.js
+++ Ch07/07_01/start/src/components/ui/Member.js
@@ -1,16 +1,18 @@
-import FaShield from 'react-icons/lib/fa/shield' 
+import FaShield from 'react-icons/lib/fa/shield'
 import { Component } from 'react'
 
 class Member extends Component {
 
 render() {
-	
+		const {name, thumbnail, email, admin, makeAdmin} = this.props;
     return (
         <div className="member">
-
-        //
-        // TODO: Create Member Component
-        //
+					<h1>{name} {(admin) ? <FaShield /> : null}</h1>
+					<a onClick={makeAdmin}>Make admin </a>
+					<img src={thumbnail} alt="profile picture" />
+					<p>
+						<a href={`mailto:${email}`}>{email}</a>
+					</p>
 
         </div>
     )
--- Ch07/07_01/start/webpack.config.js
+++ Ch07/07_01/start/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     devServer: {
         inline: true,
         contentBase: './dist',
-        port: 3000
+        port: 5000
     },
     module: {
         loaders: [
